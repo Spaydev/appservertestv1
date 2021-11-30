96cfb04b949fd53df7be839bb4388a6c99183e2a
Spaydevv <spaydevv@gmail.com>
2021-11-30 15:15:49 +0700

    make it better

--- controllers/controllerGetRefID.js
+++ controllers/controllerGetRefID.js
@@ -3,7 +3,11 @@ const modelGetRefId = require("../models/modelGetRefId")
 
 
 module.exports.getRef_id = async (req, res) => {
-    const refd = req.query.ref_id;
+    const refd = { "ref_id":req.query.ref_id,
+                    "phone":req.query.phone
+
+                    }
+
     // console.log(refd);
 
     const data = await modelGetRefId.getData(refd)
--- models/modelGetRefId.js
+++ models/modelGetRefId.js
@@ -1,26 +1,40 @@
 module.exports.getData = async (data,res) => {
+    console.log(data);
     const data_ref = [{
-        ref_id:"1234567891234",
-        first_name:"Tu",
-        last_name:"Ocha kuy"
+            "ref_id":"1234567891234",
+            "first_name":"Tu",
+            "last_name":"Ocha kuy",
+            "phone":"0123456789"
         },{
-        ref_id:"1111111111111",
-        first_name:"Spay",
-        last_name:"Zing"
+            "ref_id":"1111111111111",
+            "first_name":"Spay",
+            "last_name":"Zing",
+            "phone":"0222222222"
         },{
-        ref_id:"6969696969696",
-        first_name:"O",
-        last_name:"teepungkorn"
+            "ref_id":"6969696969696",
+            "first_name":"O",
+            "last_name":"teepungkorn",
+            "phone":"0868948956"
+        },{    
+            "ref_id": "1200100100",
+            "phone": "0877777777",
+            "date_of_birth": "2021-11-25",
+            "email": "tao_test@clicknext.com",
+            "terms_conditions_accepted": true,
+            "first_name":"tao_test",
+            "last_name":"tao_test",
         }
     ]
+    // console.log(data.ref_id+"  : "+data.phone);
     let dat_result 
     for (let index = 0; index < data_ref.length; index++) {
 
-        if(data_ref[index].ref_id === data){
+        if(data_ref[index].ref_id == data.ref_id && data_ref[index].phone == data.phone){
             dat_result =  result = {
                 ref_id:data_ref[index].ref_id,
                 first_name:data_ref[index].first_name,
                 last_name:data_ref[index].last_name,
+                phone:data_ref[index].phone,
             }
             break; 
         }
