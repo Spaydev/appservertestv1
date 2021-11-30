b72cf962b1255ca6fe59f345e971992a2159ff6a
Spaydevv <spaydevv@gmail.com>
2021-11-30 16:01:46 +0700

    make it better

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.json({ limit: "50mb" }))
 app.use("/", router)
 
 
-app.listen(3000, '0.0.0.0', async () => {
+app.listen(3000, 'localhost', async () => {
     console.log(`App listening on port 3000!`)
 })
